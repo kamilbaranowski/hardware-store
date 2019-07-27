30f909f0bdc066c2954f48b831f30d46862e1d38
kamil <tmp2206@gmail.com>
2019-07-27 22:33:43 +0200

    added fields and annotations

--- src/main/java/com/kamil/hardwarestore/model/Product.java
+++ src/main/java/com/kamil/hardwarestore/model/Product.java
@@ -1,4 +1,57 @@
 package com.kamil.hardwarestore.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Product {
+
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "netto_price")
+    private double nettoPrice;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "producer_id", referencedColumnName = "producer_id")
+    private Producer producer;
+
+
+    public Product() {
+    }
+
+    public Product(String productName, double nettoPrice, int quantity, Producer producer) {
+        this.productName = productName;
+        this.nettoPrice = nettoPrice;
+        this.quantity = quantity;
+        this.producer = producer;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getNettoPrice() {
+        return nettoPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Producer getProducer() {
+        return producer;
+    }
 }
