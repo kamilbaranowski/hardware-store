f6156c1f7366ade8ef68b9971b64fec992a85da7
kamil <tmp2206@gmail.com>
2019-07-27 22:48:27 +0200

    /products get method support

--- src/main/java/com/kamil/hardwarestore/controller/ProductController.java
+++ src/main/java/com/kamil/hardwarestore/controller/ProductController.java
@@ -1,10 +1,26 @@
 package com.kamil.hardwarestore.controller;
 
 
+import com.kamil.hardwarestore.model.ProductWrapper;
+import com.kamil.hardwarestore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ProductWrapper getProducts(){
+        return new ProductWrapper(productService.getAllProducts());
+    }
 }
--- src/main/java/com/kamil/hardwarestore/service/ProductService.java
+++ src/main/java/com/kamil/hardwarestore/service/ProductService.java
@@ -1,7 +1,26 @@
 package com.kamil.hardwarestore.service;
 
+import com.kamil.hardwarestore.dao.ProductRepository;
+import com.kamil.hardwarestore.model.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts(){
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add);
+        return products;
+    }
 }
